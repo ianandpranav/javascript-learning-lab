529fdcca1529e5c86bf38414074360b609542707
Anand Pranav <71137592+ianandpranav@users.noreply.github.com>
2026-09-14 23:54:46 +0530

    Implement age and marks eligibility conditions
    Added age and marks eligibility checks with console outputs.

---
+++ 01-js-basics/Conditions.js
@@ -0,0 +1,19 @@
+let age = 22;
+
+if (age >= 18) {
+    console.log("Eligible");
+} else {
+    console.log("Not eligible");
+}
+
+let marks = 82;
+
+if (marks >= 90) {
+    console.log("Grade: A");
+} else if (marks >= 75) {
+    console.log("Grade: B");
+} else if (marks >= 60) {
+    console.log("Grade: C");
+} else {
+    console.log("Grade: D");
+}
