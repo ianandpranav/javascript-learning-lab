0e0d4496324c719ee8bf11a1526a421da9b24337
Anand Pranav <71137592+ianandpranav@users.noreply.github.com>
2026-09-14 23:55:56 +0530

    Add loops demonstration in Loops.js

---
+++ 01-js-basics/Loops.js
@@ -0,0 +1,14 @@
+console.log("For loop:");
+
+for (let i = 1; i <= 5; i++) {
+    console.log(i);
+}
+
+console.log("While loop:");
+
+let count = 1;
+
+while (count <= 5) {
+    console.log(count);
+    count++;
+}
