96334b0d232ef0a9edf8552bc2337fcff2c6054c
Anand Pranav <71137592+ianandpranav@users.noreply.github.com>
2026-09-14 23:53:16 +0530

    Add DataTypes.js with variable declarations and logs

---
+++ 01-js-basics/DataTypes.js
@@ -0,0 +1,11 @@
+let age = 22;
+let name = "Anand";
+let isStudent = true;
+let city;
+let value = null;
+
+console.log("Age:", age);
+console.log("Name:", name);
+console.log("Student:", isStudent);
+console.log("City:", city);
+console.log("Value:", value);
